26d95b60ddd637c6dafe12b21cd6d40c1d70eafb
Luna <lunasmith@tutamail.com>
2019-02-01 20:44:45 -0300

    Menu inicial terminado

---
+++ MainMenu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+
+func _on_ButtonNewGame_pressed():
+	get_tree().change_scene("res://Level1.tscn")
+
+
+func _on_ButtonContinue_pressed():
+	get_tree().change_scene("res://SaveMenu.tscn")
+
+
+func _on_ButtonExit_pressed():
+	get_tree().quit()
---
+++ SaveMenu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_ButtonBack_pressed():
+	get_tree().change_scene("res://MainMenu.tscn")
+
+
+func _on_ButtonSlot1_pressed():
+	pass # replace with function body
+
+
+func _on_ButtonSlot2_pressed():
+	pass # replace with function body
