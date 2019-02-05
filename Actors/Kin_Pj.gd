fa1a3ceda4bdc4afc4f668c0a4feec2346a9141a
Luna <lunasmith@tutamail.com>
2019-02-04 22:27:51 -0300

    Animacion de atacar
    Agregada la animacion de atacar. Falta colision.
    Ahora agarrar llaves aumenta la cantidad de llaves globales.
    Limpieza de imagenes que ya no se usan.

--- Actors/Kin_Pj.gd
+++ Actors/Kin_Pj.gd
@@ -11,13 +11,15 @@ var vida
 var bombs
 var keys
 
+var dondeMira = "WalkDown"
+
 func _ready():
 	inventario = get_node("/root/global").inventario
 	vida = get_node("/root/global").vida
 	bombs = get_node("/root/global").bombs
 	keys = get_node("/root/global").keys
 	
-	pass
+	
 
 func _physics_process(delta):
 	_movimientos(delta)
@@ -29,20 +31,24 @@ func _physics_process(delta):
 
 	
 func _movimientos(delta):
+	
 	direction.x = int(Input.is_action_pressed("PLAYER_RIGHT"))-int(Input.is_action_pressed("PLAYER_LEFT"))
 	direction.y = int(Input.is_action_pressed("PLAYER_DOWN"))-int(Input.is_action_pressed("PLAYER_UP"))
 	
-	_actualizarAnimaciones(direction)
+	if comprobar_accion():
+		_actualizarAnimaciones(direction)
+		accion_boton()
 		
-	distance.x = speed*delta
-	distance.y = speed*delta
-	velocity.x = (direction.x*distance.x)/delta
-	velocity.y = (direction.y*distance.y)/delta
+		distance.x = speed*delta
+		distance.y = speed*delta
+		velocity.x = (direction.x*distance.x)/delta
+		velocity.y = (direction.y*distance.y)/delta
 	
-	if direction.length() > 0:
-		direction = direction.normalized() * velocity
+		if direction.length() > 0:
+			direction = direction.normalized() * velocity
 	
-	move_and_slide(velocity)
+		move_and_slide(velocity)
+	
 	
 	
 func _actualizarAnimaciones(direction):
@@ -60,6 +66,10 @@ func _actualizarAnimaciones(direction):
 #
 	if direction.y > 0:
 		$Spr_Pj/Ani_Pj.current_animation = "WalkDown"
+	
+	if $Spr_Pj/Ani_Pj.current_animation != "":
+		dondeMira = $Spr_Pj/Ani_Pj.current_animation
+	
 	
 func get_item(item):
 	var slotVacio = null
@@ -83,7 +93,59 @@ func get_item(item):
 	
 	if(item != "Vida" && item != "Bomba"):
 		inventario[slotVacio] = item
+		if(item == "Llave"):
+			keys +=1
 		return true
+
+func accion_boton():
+	
+	
+	if(Input.is_action_just_pressed("PLAYER_BUTTON_A")):
+		if global.equipoA == "Espada":
+			atacar()
+		if global.equipoA == "Bomba":
+			pass
+		if global.equipoA == "Llave":
+			pass
+		if global.equipoA == "Botas":
+			pass
+		
+	if(Input.is_action_just_pressed("PLAYER_BUTTON_B")):
+		if global.equipoB == "Espada":
+			atacar()
+		if global.equipoB == "Bomba":
+			pass
+		if global.equipoB == "Llave":
+			pass
+		if global.equipoB == "Botas":
+			pass
+			
+func atacar():
+	#animacion:
+	if dondeMira == "WalkRight" or dondeMira == "FightRight":
+		$Spr_Pj/Ani_Pj.current_animation = "FightRight"
+#
+	if dondeMira == "WalkLeft" or dondeMira == "FightLeft":
+		$Spr_Pj/Ani_Pj.current_animation = "FightLeft"
+#
+	if dondeMira == "WalkUp" or dondeMira == "FightUp":
+		$Spr_Pj/Ani_Pj.current_animation = "FightUp"
+#
+	if dondeMira == "WalkDown" or dondeMira == "FightDown":
+		$Spr_Pj/Ani_Pj.current_animation = "FightDown"
+		pass
+
+func comprobar_accion():
+	if $Spr_Pj/Ani_Pj.current_animation == "FightRight":
+		return false
+	if $Spr_Pj/Ani_Pj.current_animation == "FightLeft":
+		return false
+	if $Spr_Pj/Ani_Pj.current_animation == "FightDown":
+		return false
+	if $Spr_Pj/Ani_Pj.current_animation == "FightUp":
+		return false
+	
+	return true
 		
 func actualizar_HUD():
 	$HUD/CanvasLayer/Vida.frame = vida
--- global.gd
+++ global.gd
@@ -3,7 +3,7 @@ extends Node
 var inventario = ["Vacio", "Vacio", "Vacio", "Vacio"]
 var vida = 3
 var bombs = 0
-var keys = 1
+var keys = 0
 
 #Se inicializan con el valor del frame de la animación del inventario en el menu de pausa
 var punteroA = 4
