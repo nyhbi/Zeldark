fda2100986eae41ee1e3f6db7e1b8451971c446e
Luna <lunasmith@tutamail.com>
2019-02-05 20:41:17 -0300

    Animacion y colision de enemigos basicos
    Agregada escena de 2 enemigos. Octorok y Leever.
    Animacion y colision respectivas de cada uno.

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,19 +1,8 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
 	$ButtonNewGame.grab_focus()
 	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-
 
 func _on_ButtonNewGame_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
